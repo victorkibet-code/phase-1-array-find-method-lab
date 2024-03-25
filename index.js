5623bf30a396c6adaedeaaed11d3f9b833610b16
Victor <viqtorkibz@gmail.com>
2024-03-25 12:09:06 +0300

    completed assynment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
